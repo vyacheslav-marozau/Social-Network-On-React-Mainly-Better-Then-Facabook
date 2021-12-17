f6a65a1e049c3673286b9b11d6837edd439d4d08
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:08:13 +0300

    Add files via upload

--- react-1/src/Components/Header/Header.jsx
+++ react-1/src/Components/Header/Header.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import s from './Header.module.css'
 
+    const Header = () => {
+        return <header className={s.header}>
+        <img src='https://autoby.biz/i/news/2021/renault/new-logo.png' alt='Oner' />
+        </header>
+    }
+
+export default Header; 
