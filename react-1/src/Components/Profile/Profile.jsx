eb976aad8b5f95b861622273a92ff8e5e5eec554
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:11:26 +0300

    Add files via upload

--- react-1/src/Components/Profile/Profile.jsx
+++ react-1/src/Components/Profile/Profile.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
+/*import s from './Profile.module.css'*/
 
+
+const Profile = (props) => {
+
+    return <div>
+        <ProfileInfo />
+        <MyPosts posts={props.state.posts}/>
+
+    </div>
+}
+
+export default Profile;
