4ebcc06ddc75e8d0bbb48690d130168547a73f64
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:10:00 +0300

    Add files via upload

--- react-1/src/Components/NavBar/NavBar.jsx
+++ react-1/src/Components/NavBar/NavBar.jsx
@@ -1,1 +1,26 @@
+import React from 'react';
+import s from './NavBar.module.css'
+import {NavLink} from "react-router-dom";
 
+    const NavBar = () => {
+        return <nav className={s.nav}>
+        <div className={`${s.item} ${s.active}`}>
+            <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }
+            >Profile</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/dialogs" className = { navData => navData.isActive ? s.active : s.item }>Messages</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/news" className = { navData => navData.isActive ? s.active : s.item }>News</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
+        </div>
+            <div className={s.item}>
+                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+            </div>
+      </nav>
+     } 
+//{s.item + ' ' + s.active}
+export default NavBar; 
