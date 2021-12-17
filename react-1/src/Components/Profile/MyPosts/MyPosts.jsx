81468860f8cc06a52c3ed4da1b66ba87a0d0fb50
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:17:25 +0300

    Add files via upload

--- react-1/src/Components/Profile/MyPosts/MyPosts.jsx
+++ react-1/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import s from './MyPosts.module.css'
+import Post from './Posts/Post';
 
+    const MyPosts = (props) => {
+        let postsElements = props.posts.map(p => <Post message={p.message} likeCounter={p.likeCounter} />);
+        
+        return <div className={s.postsBlock}>
+            <h3>My Posts</h3>
+            <div>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div className={s.addButton}>
+                    <button>Add post</button>
+                </div>
+                <div className={s.remButton}>
+                    <button>Remove</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                {postsElements}
+            </div>
+        </div>
+    
+    }
+
+export default MyPosts; 
