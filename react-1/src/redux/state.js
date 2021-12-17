a0d38d1180a53a0cb23855ee481503d7a39bb19e
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:29:16 +0300

    Add files via upload

--- react-1/src/redux/state.js
+++ react-1/src/redux/state.js
@@ -1,1 +1,29 @@
 
+let state = {
+    dialogsPage: {
+        dialogs: [{id: 1, name: 'Pastor J', avatar: 'https://slm-assets.secondlife.com/assets/3649954/lightbox/orange%20dragon.jpg?1306810228'},
+            {id: 2, name: 'Joakin', avatar: 'https://www.thesun.co.uk/wp-content/uploads/2021/08/NINTCHDBPICT000675339240-e1629991954692.jpg'},
+            {id: 3, name: 'Lautaro', avatar: 'https://besthqwallpapers.com/Uploads/18-10-2020/144256/thumb2-lautaro-martinez-2020-4k-internazionale-goal.jpg'},
+            {id: 4, name: 'Marshall Bruce Mathers III', avatar: 'http://sun9-2.userapi.com/impf/8H3DA7uEYrr8aGXWqdW4ViDge53k6JGwUJGcPw/7kVvzHgqlp8.jpg?size=526x350&quality=96&sign=8a5d2e28988fba08449283070801574b&type=album'},
+            {id: 5, name: 'Ansu Fati', avatar: 'https://static.toiimg.com/thumb/msid-85834469,width-1070,height-580,imgsize-39444,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg'},
+            {id: 6, name: 'Ekaterina', avatar: 'https://i.pinimg.com/474x/4d/25/73/4d2573613020f2d11b0f662a55a2c3a0.jpg'}],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 1, message: 'How is your React-learning?'},
+            {id: 1, message: 'Yo, Broz!'},
+            {id: 1, message: 'Hi, how are you?'},
+            {id: 1, message: 'Hello, how is your bussines?!'},
+            {id: 1, message: 'Hello, I want to invite you to a conference'}]
+    },
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likeCounter: 88},
+            {id: 2, message: 'How is your React-learning?', likeCounter: 18},
+            {id: 3, message: "It's my first post!", likeCounter: 152},
+            {id: 4, message: "Yo, Bro'z!", likeCounter: 202},
+            {id: 5, message: "Yo, Bro'z!", likeCounter: 111}
+        ]
+    }
+}
+
+export default state;
