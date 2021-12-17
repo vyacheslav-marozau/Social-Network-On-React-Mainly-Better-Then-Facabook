e6dfde0e33c4e03127c3a2ce16d3d058790c152d
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-17 03:19:36 +0300

    Add files via upload

--- react-1/src/Components/Profile/MyPosts/Posts/Post.jsx
+++ react-1/src/Components/Profile/MyPosts/Posts/Post.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import s from './Post.module.css'
 
+    const Post = (props) => {
+        return  <div className={s.item}>
+                  <img src='https://d2zia2w5autnlg.cloudfront.net/46235/5ec33ee77313a-large' alt='ava1'/>
+                  <p>{props.message}</p>
+                  <div><span>Like {props.likeCounter} </span></div>
+                </div>
+                
+            //  It's our new program! Hey!  
+    
+    }
+
+export default Post; 
