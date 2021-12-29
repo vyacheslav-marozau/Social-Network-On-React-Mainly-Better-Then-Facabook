d48bf720465de979e551c426a4c71293436ca1b7
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-30 01:58:11 +0300

    Add files via upload

--- react-1/src/Components/Profile/Profile.jsx
+++ react-1/src/Components/Profile/Profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-/*import s from './Profile.module.css'*/
-
+import {updateNewPostText} from "../../redux/state";
 
 const Profile = (props) => {
-
     return <div>
         <ProfileInfo />
-        <MyPosts posts={props.state.posts}/>
+        <MyPosts posts={props.profilePage.posts}
+                 newPostText={props.profilePage.newPostText}
+                 addPost={props.addPost}
+                 updateNewPostText={props.updateNewPostText}
+        />
 
     </div>
 }
