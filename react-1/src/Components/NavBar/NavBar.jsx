abcf154fe8688e26a30b4330430a91f8fd47e064
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-30 02:01:36 +0300

    Add files via upload

--- react-1/src/Components/NavBar/NavBar.jsx
+++ react-1/src/Components/NavBar/NavBar.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import s from './NavBar.module.css'
 import {NavLink} from "react-router-dom";
+import tag from './orange-round.png'
 
-    const NavBar = () => {
+    const NavBar = (props) => {
+        /*console.log(props);*/
         return <nav className={s.nav}>
         <div className={`${s.item} ${s.active}`}>
             <NavLink to="/profile" className = { navData => navData.isActive ? s.active : s.item }
@@ -17,10 +19,21 @@ import {NavLink} from "react-router-dom";
         <div className={s.item}>
             <NavLink to="/music" className = { navData => navData.isActive ? s.active : s.item }>Music</NavLink>
         </div>
-            <div className={s.item}>
-                <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
-            </div>
+        <div className={s.item}>
+            <NavLink to="/settings" className = { navData => navData.isActive ? s.active : s.item }>Settings</NavLink>
+        </div>
+        <div className={s.item}>
+            <NavLink to="/friends" className = { navData => navData.isActive ? s.active : s.item }>Friends</NavLink>
+        </div>
+            <ul className={s.friendsList}>
+                <img src={tag} alt="Tag" />
+                <img src={tag} alt="Tag" />
+                <img src={tag} alt="Tag" />
+                <li>{props.sideBar.friends[0].name}</li>
+                <li>{props.sideBar.friends[1].name}</li>
+                <li>{props.sideBar.friends[2].name}</li>
+            </ul>
+
       </nav>
-     } 
-//{s.item + ' ' + s.active}
-export default NavBar; 
+     }
+export default NavBar;
