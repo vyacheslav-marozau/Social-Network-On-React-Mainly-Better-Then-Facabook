2a6e280d5c1c0b6942d355acceedd998f8357f30
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-30 01:59:23 +0300

    Add files via upload

--- react-1/src/Components/Profile/MyPosts/MyPosts.jsx
+++ react-1/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {createRef} from 'react';
 import s from './MyPosts.module.css'
 import Post from './Posts/Post';
 
     const MyPosts = (props) => {
         let postsElements = props.posts.map(p => <Post message={p.message} likeCounter={p.likeCounter} />);
-        
+
+        let newPostElement = createRef();
+
+        let addPost = () => {
+            props.addPost();
+            props.updateNewPostText('');
+        }
+        let onPostChange = () => {
+            let text = newPostElement.current.value;
+            props.updateNewPostText(text);
+        }
         return <div className={s.postsBlock}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <textarea onChange={onPostChange} ref={newPostElement}
+                              value={props.newPostText}/>
                 </div>
                 <div className={s.addButton}>
-                    <button>Add post</button>
+                    <button onClick={addPost} >Add post</button>
                 </div>
                 <div className={s.remButton}>
                     <button>Remove</button>
