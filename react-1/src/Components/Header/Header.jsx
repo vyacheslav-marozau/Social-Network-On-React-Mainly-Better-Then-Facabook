1768719d810bba0fde5645e95d5e6e9215957915
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-30 02:02:18 +0300

    Add files via upload

--- react-1/src/Components/Header/Header.jsx
+++ react-1/src/Components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import s from './Header.module.css'
-
+import logo from './Logo_New.png'
     const Header = () => {
         return <header className={s.header}>
-        <img src='https://autoby.biz/i/news/2021/renault/new-logo.png' alt='Oner' />
+        <img src={logo} alt='Logo' />
         </header>
     }
-
+//https://autoby.biz/i/news/2021/renault/new-logo.png
 export default Header; 
