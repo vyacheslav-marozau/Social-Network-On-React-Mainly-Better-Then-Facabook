7b65a1992e79bc24ebc3c125a9cd09daf802e3de
vyacheslav-marozau <51322851+vyacheslav-marozau@users.noreply.github.com>
2021-12-30 01:57:35 +0300

    Add files via upload

--- react-1/src/Dialogs/DialogItem/DialogItem.jsx
+++ react-1/src/Dialogs/DialogItem/DialogItem.jsx
@@ -4,7 +4,6 @@ import {NavLink} from "react-router-dom";
 
 const DialogsItem = (props) => {
     let path = "/dialogs/" + props.id;
-
     return <div className={`${s.dialog} ${s.active}`}>
         <img className={s.avatarPic} src={props.avatar} alt='avatar' />
         <NavLink className={s.dialogName} to={path}>{props.name}</NavLink>
