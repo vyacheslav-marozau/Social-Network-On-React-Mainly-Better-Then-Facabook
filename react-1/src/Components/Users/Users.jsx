918821104adaf02b0d06f328af14b9ca2a66aa0b
Vyachaslau Marozau <simonfenix@tut.by>
2022-01-28 01:26:38 +0300

    54th_lesson: жизненный цикл, life cycle methods, componentDidMount

--- react-1/src/Components/Users/Users.jsx
+++ react-1/src/Components/Users/Users.jsx
@@ -4,15 +4,10 @@ import avatar from "./Komisarenko_Avatar.png";
 import * as axios from "axios";
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-        alert('NEW!!!')
-       /* if (this.props.users.length === 0) {*/
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            });
-        // }
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        });
     }
 
     getUsers = () => {
